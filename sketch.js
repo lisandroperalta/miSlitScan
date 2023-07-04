0126ee16d72346a3828696e4b597366eaaab9f1d
Lisandro Peralta <lisandroperalta@gmail.com>
2023-07-04 00:15:39 -0300

    Responsive. Sigue trabajando a 1280x720 por cuestiones de rendmiento, pero escala a fullscreen

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let pantallaTrasera;
+
 let capture;
 let fotogramas = [];
 let feta;
@@ -8,7 +10,13 @@ let iniciar = true;
 let anchoFeta = 10;
 
 function setup() {
-  createCanvas(1280, 720);
+
+  createCanvas(innerWidth, innerHeight);
+
+
+  pantallaTrasera = createGraphics(1280, 720);
+
+
   capture = createCapture(VIDEO);
   capture.size(1280, 720);
   capture.hide();
@@ -19,19 +27,17 @@ function draw() {
 
   fotogramas.push(capture.get());
 
-  //image(capture.get(320,0,10,480),100,100);
-
 
 
   for (let i = fotogramas.length - 1; i > 0; i--) {
     feta = fotogramas[i].get(640, 0, 9, 720);
     // flip over vertical axis
-    push();
-    translate(968 - (8 * i), 0);
-    scale(-1, 1);
-    
-    image(feta,0 ,0, 9, 720);
-    pop();
+    pantallaTrasera.push();
+    pantallaTrasera.translate(968 - (8 * i), 0);
+    pantallaTrasera.scale(-1, 1);
+
+    pantallaTrasera.image(feta, 0, 0, 9, 720);
+    pantallaTrasera.pop();
 
   }
   fetaFinal = fotogramas[0].get(0, 0, 640, 720);
@@ -39,28 +45,29 @@ function draw() {
 
 
   // flip over vertical axis
-  push();
+  pantallaTrasera.push();
   // new origin
-  translate(327, 0);
-  scale(-1, 1);
+  pantallaTrasera.translate(327, 0);
+  pantallaTrasera.scale(-1, 1);
   //draws from upper right corner
-  image(fetaPrincipio, 0, 0, 0, 720);
+  pantallaTrasera.image(fetaPrincipio, 0, 0, 0, 720);
 
-  pop();
+  pantallaTrasera.pop();
 
   // flip over vertical axis
-  push();
+  pantallaTrasera.push();
   // new origin
-  translate(1280 + 320, 0);
-  scale(-1, 1);
+  pantallaTrasera.translate(1280 + 320, 0);
+  pantallaTrasera.scale(-1, 1);
   //draws from upper right corner
-  image(fetaFinal, 0, 0, 0, 720);
+  pantallaTrasera.image(fetaFinal, 0, 0, 0, 720);
 
-  pop();
-
-
+  pantallaTrasera.pop();
 
 
+
+
+  image(pantallaTrasera, 0, 0, width, height);
 
 
 
@@ -75,6 +82,10 @@ function draw() {
   }
 }
 
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 
 
